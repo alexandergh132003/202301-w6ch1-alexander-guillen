a6eae0c7648f6f234b3915b960de5667e1d7dc46
Alexander <aguillenhernandez95@gmail.com>
2023-02-15 07:47:18 +0100

    Add redux cycle for toDo

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { reducer } from "./features/toDoListSlice.ts/toDoListSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    list: reducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
